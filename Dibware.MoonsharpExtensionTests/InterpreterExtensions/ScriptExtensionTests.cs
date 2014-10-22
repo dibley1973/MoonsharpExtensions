bd7da6997b761778f5dc947f3c6ae678d9958cd6
dibley1973 <dibley1973@yahoo.co.uk>
2014-10-22 11:32:53 +0100

    20141022-1131-DWDT Added a quick test "ReadLuaFunctionIntoDictionary" to experiment with reading a Lua function in to a dictionary of delegates.

--- Dibware.MoonsharpExtensionTests/InterpreterExtensions/ScriptExtensionTests.cs
+++ Dibware.MoonsharpExtensionTests/InterpreterExtensions/ScriptExtensionTests.cs
@@ -3,6 +3,7 @@ using Dibware.MoonsharpExtensionTests.MockData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoonSharp.Interpreter;
 using System;
+using System.Collections.Generic;
 
 namespace Dibware.MoonsharpExtensionTests.InterpreterExtensions
 {
@@ -73,6 +74,60 @@ namespace Dibware.MoonsharpExtensionTests.InterpreterExtensions
             // Assert
             Assert.AreEqual(DataType.Table, resultObjectType);
         }
+
+        //[TestMethod]
+        //public void Test_AddObject_ToGlobalTable()
+        //{
+        //    // Arrange
+        //    String luaScript = MockLuaScripts.TEST_OBJECT_WITH_STRING;
+        //    Script context = new Script();
+        //    DynValue newObject = new DynValue();
+        //    DynValue newObject2 = new DynValue();
+
+        //    // Act
+        //    /* Run the script */
+        //    context.DoString(luaScript);
+
+        //    /* Add a variable */
+        //    context.Globals.Set("newKey", newObject);
+        //    var function = context.Globals["rawset"];
+        //    context.Call(function, "ZZZZ");
+        //    //context.
+
+        //    // Assert
+
+        //}
+
+        [TestMethod]
+        public void Test_ReadLuaFunctionIntoDictionary()
+        {
+            // Arrange
+            String luaScript = @"
+                -- simple add
+                function add (n1, n2)
+                    return n1 + n2
+                end";
+            Script context = new Script();
+            var expectedFunctionResult = 5.0;
+            var functionDictionary = new Dictionary<String, Delegate>();
+
+            // Act
+            context.DoString(luaScript);
+            var luaFunction = context.Globals.Get("add").Function;
+            var luaFunctionResult = luaFunction.Call(2, 3).Number; // This return result via interpreter
+
+            // Hold a reference to the Lua function in a delagate.
+            Func<double, double, double> clrFunction = (n1, n2) => luaFunction.Call(n1, n2).ToObject<int>();
+
+            // Add this into the dictionary
+            functionDictionary.Add("add", clrFunction);
+
+            // Assert
+            Assert.AreEqual(expectedFunctionResult, luaFunctionResult);
+            Assert.AreEqual(expectedFunctionResult, clrFunction(2, 3));
+            Assert.AreEqual(expectedFunctionResult, functionDictionary["add"].DynamicInvoke(2, 3));
+        }
+
 
         #endregion
     }
